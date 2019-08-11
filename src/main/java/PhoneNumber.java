f2092bd6b1b4a561ca95082df33d1d1310fc302c
sputnika <sputnika@zipcoders-MacBook-Pro-5.local>
2019-08-10 22:31:15 -0400

    new repo with previous work

---
+++ src/main/java/PhoneNumber.java
@@ -0,0 +1,17 @@
+/*
+        Given a 7 digit telephone number, print out all the possible sequences of letters that can represent the given
+        telephone number. Note that in a standard phone keypad 0 and 1 do not have any letters associated with them.
+        All 0's and 1's in the telephone number should be retained in the sequence of letters. You may use the
+        following mapping between numbers and characters 0=>0, 1=>1, 2=>abc, 3=>def, 4=>ghi, 5=>jkl, 6=>mno, 7=>pqrs,
+        8=>tuv, 9=>wxyz
+        Input:
+        Your program should read lines from standard input. Each line contains a 7 digit telephone number.
+        Output:
+        Print to standard output the words that can produce the telephone number, alphabetically sorted and comma
+        delimited.
+*/
+
+
+
+public class PhoneNumber {
+}
